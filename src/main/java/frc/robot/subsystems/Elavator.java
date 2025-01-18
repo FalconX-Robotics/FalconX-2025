553e71323d2fa8017746a645895c1ce1869fe114
alcoholicchild <152344845+alcoholicchild@users.noreply.github.com>
2025-01-18 13:43:46 -0800

    Cleaned up code

---
+++ src/main/java/frc/robot/subsystems/Elavator.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.;
+ 
+
+public class Elavator extends SubsystemBase{
+
+    SparkMax
+
+}
