ad7167b25d9b7944787bbd30544505d56f3ce5a1
FalconX <59894016+falconxhelper@users.noreply.github.com>
2025-02-20 16:26:09 -0800

    updated climb code

--- src/main/java/frc/robot/subsystems/climb/Climb.java
+++ src/main/java/frc/robot/subsystems/climb/Climb.java
@@ -6,23 +6,28 @@ import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class Climb extends SubsystemBase{
-
+public class Climb extends SubsystemBase {
     private SparkMax motor = new SparkMax(Constants.CLIMB_ID, MotorType.kBrushless);
 
     private double positiveLimit = 5;
     private double negitiveLimit = -5;
 
-    public void motorRun(double voltage){
-        motor.setVoltage(voltage);
-        if (negitiveLimit>=motor.getAbsoluteEncoder().getPosition() && voltage<0){
-        
-            motor.setVoltage(0);
-        }
-        if (positiveLimit<=motor.getAbsoluteEncoder().getPosition() && voltage>0){
+    double position = motor.getAbsoluteEncoder().getPosition();
 
-            motor.setVoltage(0);
+    public void motorRun(double voltage){
+        double finalVoltage = voltage;
+
+        boolean beyondNegative = position <= negitiveLimit;
+        boolean beyonePositive = position >= positiveLimit;
+
+        if (beyondNegative && (voltage < 0)){
+            finalVoltage = 0;
         }
+        
+        if (beyonePositive && (voltage > 0)){
+            finalVoltage = 0;
+        }
+        
+        motor.setVoltage(finalVoltage);
     }
-    
 }
